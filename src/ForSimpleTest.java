828ee72b60a4c6de6802d877d0ebe447a109e304
Andrei Navroschi <andrei.navroschi@geminisols.ro>
2015-07-13 10:24:50 +0300

    From Work
    Minor changes

--- src/ForSimpleTest.java
+++ src/ForSimpleTest.java
@@ -3,8 +3,8 @@ public class ForSimpleTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 1;
-		for (i : 5) {
+		int i;
+		for (i = 1; i<=5 ; i++) {
 			System.out.println(i);
 		}
 	}
